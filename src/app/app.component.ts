398b4a056f851c545a13a3012bd3fdff58eefdf9
lantisky <lantislexc@gmail.com>
2021-01-14 17:29:51 +1300

    update for tic tac toe (include 2 version)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent{
+  title = 'thisGame';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NbButtonModule, NbLayoutModule, NbThemeModule} from '@nebular/theme';
+import {BoardComponent} from './board/board.component';
+import {SquareComponent} from './square/square.component';
+import {GameComponent} from './game/game.component';
+import {NbEvaIconsModule} from '@nebular/eva-icons';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    SquareComponent,
+    GameComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbLayoutModule,
+    NbButtonModule,
+    NbThemeModule.forRoot({name: 'dark'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GameComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
